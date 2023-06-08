ebddc226fcd9a7b04d166a7e9034de7099310182
Isabella Campos <isabellaszcamposs@gmail.com>
2023-06-08 00:05:12 -0300

    removing unnecessaries imports

--- src/main/java/com/fiap/microgs/controller/TelemetriaController.java
+++ src/main/java/com/fiap/microgs/controller/TelemetriaController.java
@@ -1,21 +1,15 @@
 package com.fiap.microgs.controller;
 
 import com.fiap.microgs.model.Drone;
-import com.fiap.microgs.model.HistoricoVoo;
-import com.fiap.microgs.model.LicencaVoo;
 import com.fiap.microgs.model.Telemetria;
 import com.fiap.microgs.service.DroneService;
-import com.fiap.microgs.service.HistoricoService;
-import com.fiap.microgs.service.LicencaService;
 import com.fiap.microgs.service.TelemetriaService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -50,7 +44,6 @@ public class TelemetriaController {
     public ResponseEntity<Telemetria> create(@Valid @RequestBody Telemetria telemetria) {
         telemetriaService.save(telemetria);
         return new ResponseEntity<Telemetria>(telemetria, HttpStatus.CREATED);
-
     }
 
     @PostMapping("/createTelemetriaList")
--- src/main/java/com/fiap/microgs/service/TelemetriaService.java
+++ src/main/java/com/fiap/microgs/service/TelemetriaService.java
@@ -1,14 +1,11 @@
 package com.fiap.microgs.service;
 
-import com.fiap.microgs.model.LicencaVoo;
 import com.fiap.microgs.model.Telemetria;
-import com.fiap.microgs.repository.LicencaVooRepository;
 import com.fiap.microgs.repository.TelemetriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TelemetriaService {
@@ -22,21 +19,6 @@ public class TelemetriaService {
 
     public void save(Telemetria telemetria) {
         this.telemetriaRepository.save(telemetria);
-    }
-
-    public Telemetria findById(Long id) {
-        Optional<Telemetria> telemetriaOptional = telemetriaRepository.findById(id);
-        Telemetria telemetria = null;
-        if (telemetriaOptional.isPresent()) {
-            telemetria = telemetriaOptional.get();
-        } else {
-            throw new RuntimeException("Telemetria not found for this id: " + id);
-        }
-        return telemetria;
-    }
-
-    public void deleteById(Long id) {
-        this.telemetriaRepository.deleteById(id);
     }
 
 }
